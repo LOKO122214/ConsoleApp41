ae23c5590a39b6b3e33e0e442f944d87be0e38a2
2ParadoX1 <bvladev31@gmail.com>
2026-03-17 10:44:16 +0200

    1a

--- ConsoleApp41/StudentsQueriesB.cs
+++ ConsoleApp41/StudentsQueriesB.cs
@@ -8,6 +8,17 @@ namespace ConsoleApp41
 {
     class StudentsQueriesB
     {
+        static void Run()
+        {
+            var students = StudentRepository.students.OrderBy(s => s.Value).FirstOrDefault(s => s.Value == 6);
+            Console.WriteLine("Първия ученик с 6: ");
 
+            if (students.Key != null)
+            {
+                Console.WriteLine($"{students.Key} , {students.Value}");
+            }
+            else
+                Console.WriteLine("Няма такъв ученик! ");
+        }
     }
 }
